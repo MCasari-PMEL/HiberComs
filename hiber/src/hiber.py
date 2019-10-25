050cba882c80e9bd511764c3fbf72fa759993905
MCasari-PMEL <matthew.casari@noaa.gov>
2019-10-25 15:23:54 -0700

    Adding message parser and value returns.

--- hiber/src/hiber.py
+++ hiber/src/hiber.py
@@ -1,94 +1,328 @@
 import re
 import datetime
 
-class LPGAN_API:
+
+class LPGAN_API_Tx:
     def __init__(self):
         pass
 
     @staticmethod
     def set_gps_mode(enabled):
         if not isinstance(enabled, bool):
-            raise ValueError('enabled must be a boolean value')
+            raise ValueError("enabled must be a boolean value")
         if enabled:
-            param = 'true'
+            param = "true"
         else:
-            param = 'false'
+            param = "false"
 
-        return f'set_gps_mode({param})\r\n'
+        return f"set_gps_mode({param})\r\n"
 
     @staticmethod
     def do_gps_fix(input_string=None):
         if input_string == None:
-            return 'do_gps_fix\r\n'
+            return "do_gps_fix\r\n"
         else:
             if not isinstance(input_string, str):
-                raise ValueError('input_string must be a string')
-            return f'do_gps_fix(\"{input_string}\")\r\n'
+                raise ValueError("input_string must be a string")
+            return f'do_gps_fix("{input_string}")\r\n'
 
     @staticmethod
     def get_firmware_version():
-        return 'get_firmware_version\r\n'
+        return "get_firmware_version\r\n"
 
     @staticmethod
     def get_modem_info():
-        return 'get_modem_info\r\n'
+        return "get_modem_info\r\n"
 
     @staticmethod
     def set_modem_number(modem_number):
         if not isinstance(modem_number, str):
-            raise ValueError('modem_number must be string value with format \'XXXX XXXX\'')
-        pattern = re.compile(r'^\w\w\w\w \w\w\w\w$')
+            raise ValueError(
+                "modem_number must be string value with format 'XXXX XXXX'"
+            )
+        pattern = re.compile(r"^\w\w\w\w \w\w\w\w$")
         if not pattern.match(modem_number):
-            raise ValueError('modem_number must have format \'XXXX XXXX\'')
-        return f'set_modem_number({modem_number})\r\n'
+            raise ValueError("modem_number must have format 'XXXX XXXX'")
+        return f"set_modem_number({modem_number})\r\n"
 
     @staticmethod
     def get_location():
-        return 'get_location\r\n'
+        return "get_location\r\n"
 
     @staticmethod
     def set_location(latitude, longitude, elevation):
-        return f'set_location(\"{latitude:0.6f}\",\"{longitude:0.6f}\",\"{elevation:0.1f}\")\r\n'
+        return (
+            f'set_location("{latitude:0.6f}","{longitude:0.6f}","{elevation:0.1f}")\r\n'
+        )
 
     @staticmethod
     def get_datetime():
-        return 'get_datetime\r\n'
+        return "get_datetime\r\n"
 
+    @staticmethod
     def set_datetime(dt):
-        '''
+        """
         Create the set_datetime string.
         
         Args:
             dt: Datetime 
-        '''
+        """
         if not isinstance(dt, datetime.datetime):
             raise ValueError("dt must be a datetime.datetime object")
         dt_str = dt.strftime("%Y-%m-%dT%H:%M:%SZ")
-        return f'set_datetime(\"{dt_str}\")\r\n'
+        return f'set_datetime("{dt_str}")\r\n'
 
     @staticmethod
     def get_next_alarm():
-        return 'get_next_alarm\r\n'
+        return "get_next_alarm\r\n"
 
     @staticmethod
     def get_next_pass():
-        return 'get_next_pass\r\n'
+        return "get_next_pass\r\n"
 
     @staticmethod
     def go_to_sleep():
-        return 'go_to_sleep\r\n'
+        return "go_to_sleep\r\n"
 
     @staticmethod
     def toggle_payload_over_debug(is_enabled):
         if not isinstance(is_enabled, bool):
-            raise ValueError('is_enable must be a boolean value')
+            raise ValueError("is_enable must be a boolean value")
         if is_enabled:
-            return 'toggle_payload_over_debug(true)\r\n'
+            return "toggle_payload_over_debug(true)\r\n"
         else:
-            return 'toggle_payload_over_debug(false)\r\n'
+            return "toggle_payload_over_debug(false)\r\n"
 
     @staticmethod
     def set_payload(payload):
         if not isinstance(payload, int):
-            raise ValueError('payload must be an integer (number of bytes to be sent)')
-        return f'set_payload(\"{payload}\")\r\n'
+            raise ValueError("payload must be an integer (number of bytes to be sent)")
+        return f'set_payload("{payload}")\r\n'
+
+
+ERROR_CODES = {
+    "125": "Buffer Overflow",
+    "126": "Space found in command name",
+    "127": "No command name specified",
+    "128": "Generic unexpected character while parsing command name",
+    "129": "Invalid HEX value entered",
+    "130": "Unexpected character after space",
+    "131": "Unexpected character before HEX specifier",
+    "132": "Unexpected whitespace after HEX specifier",
+    "133": "Generic unexpected character found in keyword",
+    "134": "Unexpected character after keyword after space",
+    "135": "Generic unexpected character found parsing keyword",
+    "136": "Argument count exceeded",
+    "137": "Generic unexpected character found while parsing character",
+    "138": "Generic unexpected character found while parsing character",
+    "139": "End of command contained unexpected character",
+    "140": "Generic unexpected character error",
+    "141": "(internal) Unexpected string state",
+    "142": "(internal) Unexpected number state",
+    "143": "(internal) Unexpected keyword state",
+    "144": "(internal) Unexpected argument state",
+    "145": "(internal) Unexpected arguments finished state",
+    "146": "(internal) Unexpected state",
+    "147": "(internal) Unexpected hex number state",
+    "150": "Debug Message (Command parsing)",
+    "225": "HEX value not valid",
+    "226": "Unknown escape character",
+    "325": "Unknown keyword",
+    "350": "Debug Message (Argument conversion)",
+    "425": "Unknown command",
+    "426": "Too many or too little arguments passed to command",
+    "525": "Buffer overflow",
+    "600": "OK, input has been processed successfully",
+    "602": "Modem starting to sleep",
+    "603": "Cannot sleep (Wakeup pin is high)",
+    "625": "Invalid input",
+    "626": "(help command only) Unknown command",
+    "632": "GPS is disabled",
+    "633": "GPS is enabled",
+    "634": "Command not implemented",
+    "635": "Not going to sleep (wakeup within 2 seconds)",
+    "636": "Generic error code",
+}
+
+
+class LPGAN_API_Rx:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def parse_api_message(message):
+        if message.startswith("API"):
+            message = message[:message.find(')')]
+            result = message[message.find("API(") + 4 :].strip().strip(")").split(":",1)
+            if len(result) > 1:
+                result[1] = result[1].split(";")
+                result[1] = [result.lstrip() for result in result[1]]
+            else:
+                result = [result[0], []]
+        elif message.startswith("Hiber API "):
+            pass
+        else:
+            raise ValueError("Invalid API Rx string")
+        print(result)
+        return result
+
+    @staticmethod
+    def parse_api_message2(message, error_handling=False):
+        if message.startswith("API"):
+            message = message[:message.find(')')]
+            result = message[message.find("API(") + 4 :].strip().strip(")").split(":",1)
+            if len(result) > 1:
+                result[1] = result[1].split(";")
+                result[1] = [result.lstrip() for result in result[1]]
+            else:
+                result = [result[0], []]
+        elif message.startswith("Hiber API "):
+            pass
+        else:
+            raise ValueError("Invalid API Rx string")
+        print(result)
+
+        if error_handling:
+            if "600" != result[0]:
+                print(result)
+                raise ValueError(ERROR_CODES[result[0]])
+            else:
+                return result[1]
+        else:
+            return result[0]
+
+    @classmethod
+    def set_gps_mode(cls, message):
+        [error, results] = cls.parse_api_message(message)
+        if "600" != error:
+            raise ValueError(ERROR_CODES[error])
+        if results[0].strip() == "0":
+            return False
+        else:
+            return True
+
+    @classmethod
+    def do_gps_fix(cls, message):
+        [error, results] = cls.parse_api_message(message)
+        if "600" != error:
+            raise ValueError(ERROR_CODES[error])
+
+    @classmethod
+    def get_firmware_version(cls, message):
+        [error, results] = cls.parse_api_message(message)
+        if "600" != error:
+            raise ValueError(ERROR_CODES[error])
+        return {'firmware_version':results[0]}
+
+    @classmethod
+    def get_modem_info(cls, message):
+        [error, results] = cls.parse_api_message(message)
+        if "600" != error:
+            raise ValueError(ERROR_CODES[error])
+        results = {
+            'HW_TYPE_STR':results[0],
+            'HW_TYPE_INT':int(results[1]),
+            'FW_VERSION':results[2],
+            'MODEM_NO_STR':results[3],
+            'MODEM_NO_INT':int(results[4])
+        }
+        return results
+
+    @classmethod
+    def set_modem_number(cls, message):
+        [error, results] = cls.parse_api_message(message)
+        if "600" != error:
+            raise ValueError(ERROR_CODES[error])
+
+    @classmethod
+    def get_location(cls, message):
+        [error, results] = cls.parse_api_message(message)
+        if "600" != error:
+            raise ValueError(ERROR_CODES[error])
+        results = {
+            'latitude':float(results[0]),
+            'longitude':float(results[1]),
+            'seconds_since_last_fix':int(results[2]),
+            'seconds_until_next_fix':int(results[3]),
+            'altitude':float(results[4])
+        }
+
+        return results
+
+    @classmethod
+    def set_location(cls, message):
+        [error, results] = cls.parse_api_message(message)
+        if "600" != error:
+            raise ValueError(ERROR_CODES[error])
+        results = {
+            'latitude':float(results[0]),
+            'longitude':float(results[1]),
+            'seconds_since_last_fix':int(results[2]),
+            'seconds_until_next_fix':int(results[3]),
+            'altitude':float(results[4])
+        }
+
+        return results
+    @classmethod
+    def get_datetime(cls, message):
+        [error, results] = cls.parse_api_message(message)
+        if "600" != error:
+            raise ValueError(ERROR_CODES[error])
+        return {'datetime':results[0]}
+
+    @classmethod
+    def set_datetime(cls, message):
+        [error, results] = cls.parse_api_message(message)
+        if "600" != error:
+            raise ValueError(ERROR_CODES[error])
+        return {'datetime':results[0]}
+
+    @classmethod
+    def get_next_alarm(cls, message):
+        [error, results] = cls.parse_api_message(message)
+        if "600" != error:
+            raise ValueError(ERROR_CODES[error])
+        results = {
+            'alarm_id':int(results[0]),
+            'seconds_left_until_alarm':int(results[1])
+        }
+        return results
+
+    @classmethod
+    def get_next_pass(cls, message):
+        [error, results] = cls.parse_api_message(message)
+        if "600" != error:
+            raise ValueError(ERROR_CODES[error])
+        return {'seconds_left_until_pass':int(results[0])}
+
+    @classmethod
+    def go_to_sleep(cls, message):
+        [error, results] = cls.parse_api_message(message)
+        if "603" == error:
+            raise Warning(error)
+        elif ("600" != error) and ("602" != error):
+            raise ValueError(ERROR_CODES[error])
+        results = {
+            'seconds_left_until_alarm':int(results[0]),
+            'alarm_id':int(results[1])
+        }
+        return results
+
+    @classmethod
+    def toggle_payload_over_debug(cls, message):
+        [error, results] = cls.parse_api_message(message)
+        if "600" != error:
+            raise ValueError(ERROR_CODES[error])
+        return {'toggle_enabled':bool(results[0])}
+
+    @classmethod
+    def set_payload(cls, message):
+        results = cls.parse_api_message2(message, True)
+        print(results)
+
+        return {'payload_bytes':int(results[0])}
+
+        # [error, results] = cls.parse_api_message(message)
+        # if "600" != error:
+        #     raise ValueError(ERROR_CODES[error])
+        # return {'payload_bytes':int(results[0])}
---
+++ hiber/src/test_lpgan_rx.py
@@ -0,0 +1,176 @@
+
+import pytest
+import datetime as dt
+
+
+from hiber import LPGAN_API_Rx
+
+#------------------------------
+# Parsing Rx strings 
+#------------------------------
+def test_parse_api_return_should_fail_for_bad_return_string():
+    # Valid returns strings start with API or Hiber API
+    with pytest.raises(ValueError):
+        LPGAN_API_Rx.parse_api_message('Invalid entry')
+
+def test_parse_api_should_split_error_codes():
+    assert ['600', []] == LPGAN_API_Rx.parse_api_message('API(600)\r\n')
+
+def test_parse_api_should_split_error_and_single_value_return():
+    assert ['600', ['20']] == LPGAN_API_Rx.parse_api_message('API(600: 20)') 
+
+def test_parse_api_should_split_error_and_multiple_value_returns():
+    assert ['600', ['20','40','60', '80','100']] == LPGAN_API_Rx.parse_api_message('API(600: 20; 40; 60; 80;100)\r\n')
+
+def test_parse_api_should_split_and_ignore_funky_time_post_message_bs():
+    api = 'API(600: 2019-02-25T16:13:38Z) Current date: 2019-02-25T16:13:38Z\r\n'
+    results = LPGAN_API_Rx.parse_api_message(api)
+
+    assert ['600', ['2019-02-25T16:13:38Z']] == results
+
+def test_set_gps_mode_should_pass_and_return_single_boolean():
+    assert False == LPGAN_API_Rx.set_gps_mode('API(600: 0)\r\n')
+
+def test_set_gps_mode_should_fail_and_raise_exception():
+    with pytest.raises(ValueError):
+        LPGAN_API_Rx.set_gps_mode('API(525)')
+
+def test_do_gps_fix_should_pass_and_return_nothing():
+    assert None == LPGAN_API_Rx.do_gps_fix('API(600)\r\n') 
+
+def test_do_gps_fix_should_raise_exception_for_error():
+    with pytest.raises(ValueError):
+        LPGAN_API_Rx.do_gps_fix('API(136)\r\n')
+
+def test_get_firmware_version_should_return_firmware_version():
+    result =  LPGAN_API_Rx.get_firmware_version('API(600: cn-release-v1.0.0-1-gd193bbe4)')
+
+    expected_result = 'cn-release-v1.0.0-1-gd193bbe4'
+
+    assert expected_result == result['firmware_version']
+
+def test_get_firmware_should_raise_expcetion_for_error():
+    with pytest.raises(ValueError):
+        LPGAN_API_Rx.get_firmware_version('API(136)')
+
+
+def test_get_modem_info_should_return_all_modem_info():
+    api = 'API(600: GAMMA; 2; 1; 27AA 0DD8; 665456088)\r\n'
+    results = LPGAN_API_Rx.get_modem_info(api)
+
+    assert 'GAMMA' == results['HW_TYPE_STR']
+    assert 2 == results['HW_TYPE_INT']
+    assert '1' == results['FW_VERSION']
+    assert '27AA 0DD8' == results['MODEM_NO_STR']
+    assert 665456088 == results['MODEM_NO_INT']
+
+def test_get_modem_info_should_raise_exception_for_error():
+    with pytest.raises(ValueError):
+        LPGAN_API_Rx.get_modem_info('API(136)')
+
+def test_set_modem_number_should_pass_and_return_none():
+    assert None == LPGAN_API_Rx.set_modem_number('API(600)\r\n')
+
+def test_get_location_should_pass_and_return_values():
+    api = 'API(600: 0.000000; 0.000000; 136121891; -2147483648; 0.000000)\r\n'
+    results = LPGAN_API_Rx.get_location(api)
+
+    assert 0.0 == results['latitude']
+    assert 0.0 == results['longitude']
+    assert 136121891 == results['seconds_since_last_fix']
+    assert -2147483648 == results['seconds_until_next_fix']
+    assert 0.000000 == results['altitude']
+
+def test_get_location_should_raise_exception_for_error():
+    with pytest.raises(ValueError):
+        LPGAN_API_Rx.get_location('API(136)')
+
+def test_set_location_should_pass_and_return_values():
+    api = 'API(600: 54.333229; 4.212332; -157938633; -2147483648; 0.000000)\r\n'
+    results = LPGAN_API_Rx.set_location(api)
+
+    assert 54.333229 == results['latitude']
+    assert 4.212332 == results['longitude']
+    assert -157938633 == results['seconds_since_last_fix']
+    assert -2147483648 == results['seconds_until_next_fix']
+    assert 0.000000 == results['altitude']
+
+def test_set_location_should_raise_exceptions_for_errors():
+    with pytest.raises(ValueError):
+        LPGAN_API_Rx.set_location('API(225)')
+
+def test_get_datetime_should_pass_and_return_valid_datetime():
+    api = 'API(600: 2019-02-25T16:14:40Z)'
+    results = LPGAN_API_Rx.get_datetime(api)
+    assert '2019-02-25T16:14:40Z' == results['datetime']
+
+def test_get_datetime_should_raise_exception_for_error():
+    with pytest.raises(ValueError):
+        LPGAN_API_Rx.get_datetime('API(225)')
+
+def test_set_datetime_should_pass_and_return_valid_datetime():
+    api = 'API(600: 2019-02-25T16:13:38Z) Current date: 2019-02-25T16:13:38Z\r\n'
+    results = LPGAN_API_Rx.set_datetime(api)
+    assert '2019-02-25T16:13:38Z' == results['datetime']
+
+def test_set_datetime_should_raise_exception_for_errors():
+    with pytest.raises(ValueError):
+        LPGAN_API_Rx.set_datetime('API(225)')
+
+def test_get_next_alarm_should_return_valid_values():
+    api = 'API(600: 3; 39)\r\n'
+
+    results = LPGAN_API_Rx.get_next_alarm(api)
+
+    assert 3 == results['alarm_id']
+    assert 39 == results['seconds_left_until_alarm']
+
+def test_get_next_alarm_should_raise_exception_for_error():
+    with pytest.raises(ValueError):
+        LPGAN_API_Rx.get_next_alarm('API(225)')
+
+def test_get_next_pass_should_return_valid_time_until_next_pass():
+    api ='API(600: 1298)'
+    result = LPGAN_API_Rx.get_next_pass(api)
+
+    assert 1298 == result['seconds_left_until_pass']
+
+def test_get_next_pass_should_raise_exception_for_error():
+    with pytest.raises(ValueError):
+        LPGAN_API_Rx.get_next_pass('API(225)')
+
+def test_go_to_sleep_should_return_valid_values():
+    api = 'API(602: 36; 3)\r\n'
+
+    results = LPGAN_API_Rx.go_to_sleep(api)
+
+    assert 36 == results['seconds_left_until_alarm']
+    assert 3 == results['alarm_id']
+
+def test_go_to_sleep_should_raise_exception_for_error():
+    with pytest.raises(ValueError):
+        LPGAN_API_Rx.go_to_sleep('API(225)')
+
+def test_go_to_sleep_should_raise_warning_for_pin_high():
+    with pytest.raises(Warning):
+        LPGAN_API_Rx.go_to_sleep('API(603: 36; 3)')
+
+def test_toggle_payload_over_debug_should_return_valid_values():
+    api = 'API(600: 1)\r\n'
+
+    result = LPGAN_API_Rx.toggle_payload_over_debug(api)
+
+    assert True == result['toggle_enabled']
+
+def test_toggle_payload_over_debug_should_raise_exception_for_errors():
+    with pytest.raises(ValueError):
+        LPGAN_API_Rx.toggle_payload_over_debug('API(136: 1, 12)\r\n')
+
+def test_set_payload_should_return_valid_count():
+    api = 'API(600: 140)\r\n'
+    result = LPGAN_API_Rx.set_payload(api)
+    assert 140 == result['payload_bytes']
+
+def test_set_payload_should_raise_excpetion_for_error():
+    with pytest.raises(ValueError):
+        LPGAN_API_Rx.set_payload('API(127: 127)\r\n') 
